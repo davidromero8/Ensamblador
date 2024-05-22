f9c13c522c959471160214064e777e08ad279717
davidromero8 <112227937+davidromero8@users.noreply.github.com>
2024-05-22 04:42:19 -0700

    Add files via upload

---
+++ multiplicacion/mult.asm
@@ -0,0 +1,26 @@
+section .bss
+resultado resb 1 ;guarda un espacio de 1 byte para el resultado
+
+section .text
+	global _start
+
+_start:
+
+mov bx, 4
+mov dx, 0 ;residuo, agregar el residuo en el video
+mov ax, 5
+
+mul	bx 
+
+add ax, 48 ;el 48 en Ascii es 0 asi al sumar el resultado que esta en ax un 0
+mov [resultado], ax ;para cambiar el contenido de la direccion de memoria no la direcion de memeoria en si
+
+mov eax, 4 ;aviso al sistema operativo que voy a escribir
+mov ebx, 1 ;aviso al sistema operativo que voy a imprimir en pantalla
+mov ecx, resultado
+mov edx, 1 ; el numero de bytes que se van a imprimir en pantalla
+int 0x80
+
+mov eax, 1
+mov ebx, 0 ;para indicar al sistema operativo que el programa termino
+int 0x80
